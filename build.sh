067db00d3629fbacc7e160d9cfb1d9dedd61e71f
jpitt <jpitt@thoughtmachine.net>
2026-09-21 09:20:46 +0100

    update deployment

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -x
 npm run build
 cp main.js manifest.json styles.css ../james_notes/.obsidian/plugins/obsidian-kanban
+cp main.js manifest.json styles.css ../tm_notes/.obsidian/plugins/obsidian-kanban
